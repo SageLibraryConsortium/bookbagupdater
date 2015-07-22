c5a3aa76cd86ad8ce8e65cae09370376528329cc
opensrf <opensrf@app3sage.eou.edu>
2015-07-22 10:02:44 -0700

    Update The Repo

--- bin/bookbag_update.pl
+++ bin/bookbag_update.pl
@@ -44,14 +44,15 @@ if(! -e $xmlconf)
 	our $mobUtil = new Mobiusutil();  
 	our $log;
 	our $dbHandler;	
-    our @updatetypes = ('newitems','recentreturned','last14daytopcirc','newyoungadult','newkids');
+    our @updatetypes = ('sagenewitems','newitems','recentreturned','last14daytopcirc','newyoungadult','newkids');
 
-  # These are the 5 types:
-  # Newly cataloged items (regardless of age of bib)    (newitems)
-  # Newly cataloged YA items (based on shelving loc)    (newyoungadult)
-  # Newly cataloged Kids items (based on shelving loc)  (newkids)
-  # Recently returned (last 100 items returned)         (recentreturned)
-  # Last 14 days, top 100 circulated titles             (last14daytopcirc)
+  # These are the 6 types:
+  # Newly cataloged items in SAGE (regardless of age of bib)    (sagenewitems)
+  # Newly cataloged items by list member OU.			(newitems)
+  # Newly cataloged YA items (based on shelving loc)    	(newyoungadult)
+  # Newly cataloged Kids items (based on shelving loc)  	(newkids)
+  # Recently returned (last 100 items returned)        		(recentreturned)
+  # Last 14 days, top 100 circulated titles			(last14daytopcirc)
 	
 my $dt = DateTime->now(time_zone => "local"); 
 my $fdate = $dt->ymd; 
@@ -84,18 +85,22 @@ foreach(@results)
 	{
 		$inserts = updatebagNewItems($bucketID,$ous);
 	}
+	elsif($des eq 'sagenewitems')
+	{
+		$inserts = updatebagSageNewItems($bucketID,$ous);
+	}
 	elsif($des eq 'recentreturned')
 	{
 		$inserts = updatebagRecentReturned($bucketID,$ous);
 	}
-  elsif($des eq 'newyoungadult')
-  {
-    $inserts = updatebagNewYoungAdultItems($bucketID,$ous);
-  }
-  elsif($des eq 'newkids')
-  {
-    $inserts = updatebagNewKidsItems($bucketID,$ous);
-  }
+	elsif($des eq 'newyoungadult')
+	{
+		$inserts = updatebagNewYoungAdultItems($bucketID,$ous);
+	}
+	elsif($des eq 'newkids')
+	{
+		$inserts = updatebagNewKidsItems($bucketID,$ous);
+	}
 	elsif($des eq 'last14daytopcirc')
 	{
 		$inserts = updatebag14daytopcirc($bucketID,$ous);				
@@ -118,7 +123,7 @@ foreach(@results)
 
 $log->addLogLine(" ---------------- Script End ---------------- ");	
 	
-sub updatebagNewItems
+sub updatebagSageNewItems
 {
 	my $id = @_[0];
 	my $ous = @_[1];
@@ -128,20 +133,16 @@ SELECT * FROM
  SELECT DISTINCT \"REC\",
  (SELECT MAX(CREATE_DATE::DATE) FROM ASSET.COPY WHERE CALL_NUMBER = (SELECT MAX(ID) FROM ASSET.CALL_NUMBER WHERE 
 RECORD=\"REC\")
-AND CIRC_LIB 
-IN(1,2,3,4,5,6,9,101,102,103,104,105,107,108,109,110,111,112,114,116,118,119,120,121,122,123,124,125,126,127,128,129,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,172,173,174,175,176,177,178,179,180,181,182,185,186,187,189,190,191,192,193,194,195,196,197,198,199,202,203,204,206,207,208,209,210,211,212,213,214,216,232,233,234,235,236,237,238,239,240,241)) 
+) 
 \"THEDATE\"
 
   FROM
 (
 SELECT (SELECT RECORD FROM ASSET.CALL_NUMBER WHERE ID=A.CALL_NUMBER AND RECORD>0 AND RECORD IS NOT NULL) 
-\"REC\",CREATE_DATE::DATE FROM ASSET.COPY  A WHERE CIRC_LIB 
-IN(1,2,3,4,5,6,9,101,102,103,104,105,107,108,109,110,111,112,114,116,118,119,120,121,122,123,124,125,126,127,128,129,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,172,173,174,175,176,177,178,179,180,181,182,185,186,187,189,190,191,192,193,194,195,196,197,198,199,202,203,204,206,207,208,209,210,211,212,213,214,216,232,233,234,235,236,237,238,239,240,241)
-AND LOCATION IN(SELECT ID FROM ASSET.COPY_LOCATION WHERE OWNING_LIB 
-IN(1,2,3,4,5,6,9,101,102,103,104,105,107,108,109,110,111,112,114,116,118,119,120,121,122,123,124,125,126,127,128,129,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,172,173,174,175,176,177,178,179,180,181,182,185,186,187,189,190,191,192,193,194,195,196,197,198,199,202,203,204,206,207,208,209,210,211,212,213,214,216,232,233,234,235,236,237,238,239,240,241) 
-AND OPAC_VISIBLE AND HOLDABLE AND 
-CIRCULATE and NAME 
-NOT SIMILAR TO '%(Magazines|MAGAZINES|PERIODICALS|ADULT MAGAZINES|CHILDREN''S MAGAZINES|ADULTOS ESPANOL REVISTAS)%') 
+\"REC\",CREATE_DATE::DATE FROM ASSET.COPY  A WHERE LOCATION IN(SELECT ID FROM ASSET.COPY_LOCATION WHERE
+OPAC_VISIBLE AND HOLDABLE AND 
+CIRCULATE and NAME NOT IN ('Magazines','MAGAZINES','PERIODICALS','Periodicals','ADULT MAGAZINES','Stacks','Storage','CHILDREN''S 
+MAGAZINES','ADULTOS ESPANOL REVISTAS')) 
 AND OPAC_VISIBLE AND HOLDABLE AND 
 CIRCULATE AND ID != -1::BIGINT
 ORDER BY
@@ -166,6 +167,52 @@ LIMIT 100
 		}
 	}
 	return $inserts;
+}
+
+sub updatebagNewItems
+{
+  my $id = @_[0];
+  my $ous = @_[1];
+  my $query = "
+SELECT * FROM
+(
+ SELECT DISTINCT \"REC\",
+ (SELECT MAX(CREATE_DATE::DATE) FROM ASSET.COPY WHERE CALL_NUMBER = (SELECT MAX(ID) FROM ASSET.CALL_NUMBER WHERE
+RECORD=\"REC\")
+ AND CIRC_LIB IN($ous)) \"THEDATE\"
+
+  FROM
+(
+SELECT (SELECT RECORD FROM ASSET.CALL_NUMBER WHERE ID=A.CALL_NUMBER AND RECORD>0 AND RECORD IS NOT NULL)
+\"REC\",CREATE_DATE::DATE FROM ASSET.COPY  A WHERE CIRC_LIB IN($ous)
+AND LOCATION IN(SELECT ID FROM ASSET.COPY_LOCATION WHERE OWNING_LIB IN($ous) AND OPAC_VISIBLE AND HOLDABLE AND
+CIRCULATE and NAME NOT IN
+('Magazines','MAGAZINES','PERIODICALS','ADULT MAGAZINES','Stacks','Storage','CHILDREN''S MAGAZINES','ADULTOS ESPANOL 
+REVISTAS'))
+AND OPAC_VISIBLE AND HOLDABLE AND
+CIRCULATE AND ID != -1::BIGINT
+ORDER BY
+CREATE_DATE::DATE DESC LIMIT 300
+) AS B
+) AS C
+where C.\"THEDATE\" IS NOT NULL
+ORDER BY C.\"THEDATE\" DESC
+LIMIT 100
+";
+
+  $log->addLine($query);
+  my @results = @{$dbHandler->query($query)};
+  my $inserts = "";
+  foreach(@results)
+  {
+    my $row = $_;
+    my @row = @{$row};
+    if(length($mobUtil->trim(@row[0])) >0)
+    {
+      $inserts.="($id,".@row[0]."),";
+    }
+  }
+  return $inserts;
 }
 
 sub updatebagRecentReturned
@@ -224,8 +271,7 @@ SELECT (SELECT RECORD FROM ASSET.CALL_NUMBER WHERE ID=A.CALL_NUMBER AND RECORD>0
 \"REC\",CREATE_DATE::DATE FROM ASSET.COPY  A WHERE CIRC_LIB 
 IN($ous)
 AND LOCATION IN(SELECT ID FROM ASSET.COPY_LOCATION WHERE OWNING_LIB IN($ous) AND OPAC_VISIBLE AND HOLDABLE AND 
-CIRCULATE and NAME
-SIMILAR TO '%(YA|Young Adult|YOUNG ADULT)%') AND OPAC_VISIBLE AND HOLDABLE AND
+CIRCULATE and NAME SIMILAR TO '%(YA|Young Adult|YOUNG ADULT)%') AND OPAC_VISIBLE AND HOLDABLE AND
 CIRCULATE AND ID != -1::BIGINT
 ORDER BY
 CREATE_DATE::DATE DESC LIMIT 300
@@ -268,8 +314,11 @@ RECORD=\"REC\")
 SELECT (SELECT RECORD FROM ASSET.CALL_NUMBER WHERE ID=A.CALL_NUMBER AND RECORD>0 AND RECORD IS NOT NULL) 
 \"REC\",CREATE_DATE::DATE FROM ASSET.COPY A WHERE CIRC_LIB 
 IN($ous)
-AND LOCATION IN(SELECT ID FROM ASSET.COPY_LOCATION WHERE NAME SIMILAR TO 
-'%(Children|CHILDREN|JUV|JUVENILE|Juvenile|YOUTH|Youth)%' AND OPAC_VISIBLE) 
+AND LOCATION IN(SELECT ID FROM ASSET.COPY_LOCATION WHERE
+NAME SIMILAR TO '%(Children|CHILDREN|JUV|JUVENILE|Juvenile|YOUTH|Youth)%' 
+AND NAME NOT IN ('Juvenile Magazines','CHILDREN''S MAGAZINES','MAGAZINES','Magazines') 
+AND 
+OPAC_VISIBLE) 
 AND 
 OPAC_VISIBLE AND HOLDABLE AND CIRCULATE AND ID != -1::BIGINT
 ORDER BY
@@ -309,8 +358,10 @@ AND RECORD>0 AND RECORD IS NOT NULL) \"REC\"
 FROM ACTION.CIRCULATION  A 
 WHERE CIRC_LIB IN($ous) AND 
 (TARGET_COPY IN(SELECT ID FROM ASSET.COPY WHERE LOCATION IN(SELECT ID FROM ASSET.COPY_LOCATION WHERE OWNING_LIB 
-IN($ous) AND OPAC_VISIBLE AND HOLDABLE AND CIRCULATE) AND OPAC_VISIBLE AND HOLDABLE AND CIRCULATE AND ID != 
--1::BIGINT)) AND
+IN($ous)
+AND NAME NOT SIMILAR TO '%(Magazines|MAGAZINES|PERIODICALS|ADULT MAGAZINES|CHILDREN''S MAGAZINES|ADULTOS ESPANOL 
+REVISTAS)%'
+AND OPAC_VISIBLE AND HOLDABLE AND CIRCULATE) AND OPAC_VISIBLE AND HOLDABLE AND CIRCULATE AND ID != -1::BIGINT)) AND
 XACT_START > NOW() - \$\$14 DAYS\$\$::INTERVAL
 ) AS B 
 GROUP BY \"REC\"
